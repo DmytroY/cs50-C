79eb0a35aa849ebf71910fb419e06bce64152d2d
Dmytro <dmitry.yakovenko@gmail.com>
2021-07-04 07:08:52 +0300

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -9,19 +9,19 @@
 int main(int argc, string argv[])
 {
     int i, k;
-    string keys=argv[1], plaintext;
+    string keys = argv[1], plaintext;
     char c;
 
     // Must be one argument in command line
-    if (argc!=2)
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1; 
     }
     // A argument must be a digit  
-    for (i=0; i<strlen(keys); i++)
+    for (i = 0; i < strlen(keys); i++)
     {
-        if(isdigit(keys[i]) == 0)
+        if (isdigit(keys[i]) == 0)
         {
             printf("Usage: ./caesar key\n");
             return 2;
@@ -29,39 +29,39 @@ int main(int argc, string argv[])
     }
 
     // Convert string to int
-    k=atoi(keys);
-    k=k%26;
+    k = atoi(keys);
+    k = k % 26;
     // Prompt for plaintext
-    plaintext=get_string("plaintext:  ");
+    plaintext = get_string("plaintext:  ");
     printf("ciphertext: ");
-    for (i=0; i<strlen(plaintext); i++)
+    for (i = 0; i < strlen(plaintext); i++)
     {
         // Uppercase letter range
-        if (plaintext[i]>=65 && plaintext[i]<=90)
+        if (plaintext[i] >= 65 && plaintext[i] <= 90)
         {
             //shift 
-            c=plaintext[i]+k;
+            c = plaintext[i] + k;
             // if jump over range
-            if (c<65 || c>90)
+            if (c < 65 || c > 90)
             {
-                c=c-26;
+                c = c - 26;
             }
             goto print;
         }
         // Lowercase letter range
-        if (plaintext[i]>=97 && plaintext[i]<=122)
+        if (plaintext[i] >= 97 && plaintext[i] <= 122)
         {
-            c=plaintext[i]+k;
-            if (c<97 || c>122)
+            c = plaintext[i] + k;
+            if (c < 97 || c > 122)
             {
-                c=c-26;
+                c = c - 26;
             }
             goto print;
         }
         // Other simbols
-        c=plaintext[i];
+        c = plaintext[i];
 print:        
-        printf("%c",c);
+        printf("%c", c);
     }
     printf("\n");
 }
